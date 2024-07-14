04b0199441bac6c8b2f4356948c5038ff8fd2b8e
ilyas <ilyasalqordhowi@gmail>
2024-07-14 11:55:10 +0700

    upgrade  css

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -184,3 +184,13 @@ footerData.forEach((section) => {
 
   footer.appendChild(sectionDiv);
 });
+// pop up create event
+// const overlay = document.getElementsByClassName("content-create");
+// const addBtn = document.getElementById("button");
+// const form = overlay.item(0).getElementsByTagName("form").item(0);
+// addBtn.addEventListener("click", () => {
+//   overlay.item(0).classList.toggle("hide");
+// });
+// form.addEventListener("submit", () => {
+//   overlay.item(0).classList.toggle("hide");
+// });
